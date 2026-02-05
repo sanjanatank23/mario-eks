5b2911b8590c2d17bb82facf993dc231f65ef068
Sanjana Tank <sanjana142@gmail.com>
2026-02-05 13:04:24 +0530

    Add GitHub OIDC IAM role for CI/CD pipeline

---
+++ terraform/modules/iam/github_oidc.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name = "github-actions-mario-eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:sanjanatank23/mario-eks:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "github_ecr_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_role_policy_attachment" "github_eks_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
